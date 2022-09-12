f4837a7b500a0d42a677400627675c89ad0bc5c3
SaiAravindVemuri <saiaravindvemuri11@gmail.com>
2022-09-12 13:48:15 +0530

    first commit for sample test

---
+++ demo-project/src/main/java/com/example/demo/sample.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/sample")
+public class sample {
+	
+	@GetMapping(value = "/publish")
+    public String sendMessageToKafkaTopic() {
+        return "deployment is done";
+    }
+	
+
+}
